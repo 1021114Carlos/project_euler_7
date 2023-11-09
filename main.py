4d417562332c95be3deca0d3a8ebae6a80f54e85
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-11-09 09:32:43 -0700

    Add files via upload
    downloaded file form online gdb to personal computer. uploaded file to github.

---
+++ main.py
@@ -0,0 +1,12 @@
+""" 
+The numbers 2, 3, 5, 7, 11, 13 are prime. 13 is
+in index position 6.
+What number is in index postion 10001
+"""
+
+primeFound = False
+indexOfPrime = []
+indexes = 10001
+naturalNum = 1
+while primeFound:
+    
